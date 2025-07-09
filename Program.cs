527f33b67a489008ee8ac0e4023f6caf4cc79f69
jeromecagado <jerome.cagado@outlook.com>
2025-07-09 08:28:25 -0700

    Added some error checking and cleaner output.

--- Program.cs
+++ Program.cs
@@ -17,9 +17,10 @@ namespace Assignments1._2._3
                 Console.WriteLine("\"s\" for subtraction");
                 Console.WriteLine("\"m\" for multiplication");
                 Console.WriteLine("\"d\" for division");
-                Console.WriteLine("\"e\" to exit");
+                Console.WriteLine("\"e\" to exit\n");
 
                 char value = Convert.ToChar(Console.ReadLine().ToLower());
+                Console.WriteLine();
 
                 switch (value)
                 {
@@ -35,8 +36,12 @@ namespace Assignments1._2._3
                     case 'd':
                         Division();
                         break;
-                    default:
+                    case 'e':
+                        Console.WriteLine("You are exiting the program. Goodbye!");
                         flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("You entered the wrong key. Try again.");
                         break;
                 }
             } while (flag);
@@ -44,36 +49,36 @@ namespace Assignments1._2._3
         
         public static void Addition()
         {
-            Console.WriteLine("Please enter the first numbers!");
+            Console.WriteLine("Please enter the first number");
             int firstIntegerNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the second number");
             int secondIntegerNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"The answer is:{firstIntegerNumber + secondIntegerNumber}");
+            Console.WriteLine($"The answer is:{firstIntegerNumber + secondIntegerNumber}\n");
         }
 
         public static void Subtraction()
         {
-            Console.WriteLine("Please enter the first numbers!");
+            Console.WriteLine("Please enter the first number");
             int firstSubtractedNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the second number");
             int secondSubtractedNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"The answer is: {firstSubtractedNumber - secondSubtractedNumber}");
+            Console.WriteLine($"The answer is: {firstSubtractedNumber - secondSubtractedNumber}\n");
         }
 
         public static void Multiplication()
         {
-            Console.WriteLine("Please enter the first numbers!");
+            Console.WriteLine("Please enter the first number");
             int firstMultipliedNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the second number");
             int secondMultipliedNumber = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"The answer is: {firstMultipliedNumber * secondMultipliedNumber}");
+            Console.WriteLine($"The answer is: {firstMultipliedNumber * secondMultipliedNumber}\n");
         }
 
         public static void Division()
         {
-            Console.WriteLine("Please enter the first numbers!");
+            Console.WriteLine("Please enter the first number");
             double firstDividedNumber = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter the second number not zero.");
+            Console.WriteLine("Please enter the second number.");
             double secondDividedNumber = Convert.ToDouble(Console.ReadLine());
             while (secondDividedNumber == 0)
             {
@@ -81,7 +86,8 @@ namespace Assignments1._2._3
                 secondDividedNumber = Convert.ToDouble(Console.ReadLine());
             }
            
-            Console.WriteLine($"The answer is: {firstDividedNumber / secondDividedNumber}");
+            Console.WriteLine($"The answer is: {firstDividedNumber / secondDividedNumber}\n");
+            
         }
     }
 }
